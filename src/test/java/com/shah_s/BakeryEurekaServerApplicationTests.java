ecc908aa86314efa564a46b7c318da845caebe1e
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 21:04:07 +0530

    remove: removed the test container will update later

--- src/test/java/com/shah_s/BakeryEurekaServerApplicationTests.java
+++ src/test/java/com/shah_s/BakeryEurekaServerApplicationTests.java
@@ -2,10 +2,8 @@ package com.shah_s;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 @TestPropertySource(properties = {
         "eureka.client.register-with-eureka=false",
