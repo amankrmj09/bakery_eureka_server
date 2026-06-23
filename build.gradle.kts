a686747a5015971a30fa797aa55e120bb8aa9410
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 20:23:47 +0530

    Update: Gradle configuration to use springboot version 3.5.15 and spring cloud version 2025.0.3 and added cloud client dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.5.6"
+    id("org.springframework.boot") version "3.5.15"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.graalvm.buildtools.native") version "0.10.6"
-    id("org.asciidoctor.jvm.convert") version "3.3.2"
+//    id("org.graalvm.buildtools.native") version "0.10.6"
+//    id("org.asciidoctor.jvm.convert") version "4.0.3"
 }
 
-group = "com.shah_s"
-version = "0.0.1-SNAPSHOT"
-description = "bakery_eureka_server"
+group = "com.dev_of_blue"
+version = "1.0.0"
+description = "bakery_eureka_server for Spring Cloud"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
@@ -20,18 +20,19 @@ repositories {
     mavenCentral()
 }
 
-extra["snippetsDir"] = file("build/generated-snippets")
-extra["springCloudVersion"] = "2025.0.0"
+// extra["snippetsDir"] = file("build/generated-snippets")
+extra["springCloudVersion"] = "2025.0.3"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    // runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.testcontainers:junit-jupiter")
+    // testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    // testImplementation("org.testcontainers:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -45,15 +46,15 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-tasks.test {
-    outputs.dir(project.extra["snippetsDir"]!!)
-}
+// tasks.test {
+//     outputs.dir(project.extra["snippetsDir"]!!)
+// }
+//
+// tasks.asciidoctor {
+//     inputs.dir(project.extra["snippetsDir"]!!)
+//     dependsOn(tasks.test)
+// }
 
-tasks.asciidoctor {
-    inputs.dir(project.extra["snippetsDir"]!!)
-    dependsOn(tasks.test)
-}
-
-tasks.named("processAot") {
-    enabled = false
-}
+//tasks.named("processAot") {
+//    enabled = false
+//}
